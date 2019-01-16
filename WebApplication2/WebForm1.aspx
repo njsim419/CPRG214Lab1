865ed6488e8895241d811a4ccf181b29426b3862
Nicholas Sim <783641@ACDM.DS.SAIT.CA>
2019-01-16 15:10:58 -0700

    :)

---
+++ WebApplication2/WebForm1.aspx
@@ -0,0 +1,50 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebApplication2.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head runat="server">
+        <title>Temperature Calculator</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <link rel="stylesheet" href="Styles/Main.css" />
+    </head>
+    <body>
+        <div Class="jumbotron text-center">
+            <h2 style="color:#fff;">Temperature Calculator</h2>
+        </div>
+        <div class="container" style="margin-left:10%; margin-right:10%">
+                <form id="form1" runat="server">
+                    <div class="form-group">
+                       <label for="FromList" class="mr-sm-2">From:</label>
+                       <asp:DropDownList ID="FromList" class="form-control mb-2 mr-sm-2" runat="server">
+                              <asp:ListItem Selected="True">Celsius</asp:ListItem>  <%-- Sets Celscius as the default type --%>
+                              <asp:ListItem>Fahrenheit</asp:ListItem>
+                              <asp:ListItem>Kelvin</asp:ListItem>
+                       </asp:DropDownList>
+                       <label for="ToList" class="mr-sm-2">Converting To:</label>
+                           <asp:DropDownList ID="ToList" class="form-control mb-2 mr-sm-2" runat="server"> 
+                               <asp:ListItem>Celsius</asp:ListItem>
+                               <asp:ListItem Selected="True">Fahrenheit</asp:ListItem> <%-- Sets Fahrenheit as the default type --%>
+                               <asp:ListItem>Kelvin</asp:ListItem>
+                            </asp:DropDownList>
+            </div>
+            <div class="form-group"> 
+                 <label for="InputTemp">Input Temperature:</label>
+                     <asp:TextBox ID="InputTemp" class="form-control" runat="server"></asp:TextBox>
+                     &nbsp;<asp:RangeValidator runat="server" id="rngDate" controltovalidate="InputTemp" type="Double" minimumvalue="-1000" maximumvalue="1000"
+                     errormessage="Error: Your number must be between -1000 and 1000" CssClass="auto-style8" style="color: #CC0000" /> <%-- Verifies that the number inputed is between -1000 and 1000 --%>
+            </div>
+                     <label for="InputTemp">Output Temperature:</label>
+                       <asp:TextBox ID="OutputField" runat="server" class="form-control"></asp:TextBox>
+                       </br>
+                       <asp:Button ID="ButtonClear" runat="server" Text="Clear" Class="btn btn-warning btn-block" OnClick="ButtonClear_Click" /> <%-- Clears the values. --%>
+                       </br>
+                       <asp:Button ID="ButtonCalculate" runat="server" OnClick="ButtonCalculate_Click" Class="btn btn-success btn-block" Text="Calculate"/>     
+            </form>
+            </div>
+    </body>
+</html>
---
+++ WebApplication2/WebForm1.aspx.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication2
+{
+    /* NAME: Nicholas Sim
+     * CLASS: CPRG214 ASP.NET LAB: I
+     * DATE: January 16, 2019
+     */
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void ButtonCalculate_Click(object sender, EventArgs e)
+        {
+            OutputField.Text = "Testing";
+            char From = 'C';
+            char To = 'C';
+
+            double temp;
+            bool inputTemp = Double.TryParse(InputTemp.Text, out temp);
+            if (FromList.SelectedIndex == 0) //Swap the type for calculation to the type that is required for the calculations
+            {
+                From = 'C';
+            }
+            if (FromList.SelectedIndex == 1)
+            {
+                From = 'F';
+            }
+            if (FromList.SelectedIndex == 2)
+            {
+                From = 'K';
+            }
+            if (ToList.SelectedIndex == 0)
+            {
+                To = 'C';
+            }
+            if (ToList.SelectedIndex == 1)
+            {
+                To = 'F';
+            }
+            if (ToList.SelectedIndex == 2)
+            {
+                To = 'K';
+            }
+            OutputField.Text = ""+ Math.Round(CalculateTemperature(From, To, temp), 2); //Formats to only have 2 decimals Ex: 12.04
+        }
+
+        public double CalculateTemperature(char TempIn, char TempOut, double input)
+        {
+            if(TempIn == 'C' && TempOut == 'C') //Calculate if the Temperature is Celsius to Celsius.
+            {
+                return input;
+            }
+            if (TempIn == 'F' && TempOut == 'F') //Calculate if the Temperature is Fahrenheit to Fahrenheit.
+            {
+                return input;
+            }
+            if (TempIn == 'K' && TempOut == 'K') //Calculate if the Temperature is Kalvin to Kalvin.
+            {
+                return input;
+            }
+            if (TempIn == 'C' && TempOut == 'F') //Calculate if the Celsius is Fahrenheit to Kalvin.
+            {
+                return ((input * 9 / 5) + 32);
+            }
+            if (TempIn == 'C' && TempOut == 'K') //Calculate if the Celsius is Kalvin to Kalvin.
+            {
+                return input + 273.15;
+            }
+            if (TempIn == 'F' && TempOut == 'C') //Calculate if the Temperature is Fahrenheit to Celsius.
+            {
+                return ((input - 32) * 5) / 9;
+            }
+            if (TempIn == 'F' && TempOut == 'K') //Calculate if the Temperature is Fahrenheit to Kalvin.
+            {
+                return ((input - 32) * 5/9 + 273.15);
+            }
+            if (TempIn == 'K' && TempOut == 'C') //Calculate if the Temperature is Kalvin to Celsius.
+            {
+                return (input - 273.15);
+            }
+            if (TempIn == 'K' && TempOut == 'F') //Calculate if the Temperature is Kalvin to Fahrenheit.
+            {
+                return ((input - 273.15) * 9 / 5 + 32);
+            }
+            else
+            {
+                return input;
+            }
+        }
+
+        protected void ButtonClear_Click(object sender, EventArgs e)
+        {
+            OutputField.Text = string.Empty; //Clear all the fields.
+            InputTemp.Text = string.Empty;
+        }
+    }
+}
